870cee162f17550d8db1a9a62758205e03279ba4
vijayalaxmiponnaboina <ponnaboinarahulyadav@gmail.com>
2026-05-27 16:28:57 +0530

    Add files via upload

---
+++ binarysearch.py
@@ -0,0 +1,14 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        n=len(nums)
+        l=0
+        r=n-1
+        while l<=r:
+            mid=(l+r)//2
+            if nums[mid]==target:
+                return mid
+            elif target<nums[mid]:
+                r=mid-1
+            else:
+                l=mid+1
+        return -1
